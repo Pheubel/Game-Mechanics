3dad7196c5ef3a6362cfdbc46b5e476b57798ce4
Pheubel <luca.guagliardo@live.nl>
2018-09-18 21:10:01 +0200

    updated unity + made basic scene to test objects

---
+++ Game Mechanics/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float MovementSpeed;
+    public float JumpPower;
+    private new Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        rigidbody.velocity += 
+            new Vector2(
+                Input.GetAxisRaw("Horizontal") * Time.fixedDeltaTime * MovementSpeed, 
+                Input.GetKeyDown(KeyCode.Space) ? JumpPower : 0
+                );
+
+    }
+
+    public enum MobilityState
+    {
+        InAir,
+        OnGround,
+        OnWall
+    }
+
+    public enum ContactState
+    {
+        Vurnable,
+        Damaged,
+        Invincible
+    }
+}
