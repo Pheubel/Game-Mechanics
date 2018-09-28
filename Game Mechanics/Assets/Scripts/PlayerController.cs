782ef806a16cf19662008233cb73f2a95c04733b
Pheubel <luca.guagliardo@live.nl>
2018-09-28 19:39:51 +0200

    made a platform to walk on

--- Game Mechanics/Assets/Scripts/PlayerController.cs
+++ Game Mechanics/Assets/Scripts/PlayerController.cs
@@ -2,31 +2,98 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
-    public float MovementSpeed;
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]private float MovementAcceleration;
     public float JumpPower;
-    private new Rigidbody2D rigidbody;
+    public float Gravity;
+    [SerializeField] private MobilityStates _mobilityState;
+    [Range(0, 1), Tooltip("Determines how much energy is conserved when walking along the ground.")]
+    public float Friction;
 
-	// Use this for initialization
-	void Start () {
-        rigidbody = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
+    [SerializeField] private float _velocityX;
+    [SerializeField] private float _velocityY;
 
-        rigidbody.velocity += 
-            new Vector2(
-                Input.GetAxisRaw("Horizontal") * Time.fixedDeltaTime * MovementSpeed, 
-                Input.GetKeyDown(KeyCode.Space) ? JumpPower : 0
-                );
+    [SerializeField] private float _maxVelocityX;
+    [SerializeField] private float _maxVelocityY;
 
+    [Tooltip("casts ray downward to check ground!")]
+    [SerializeField] private float _downwardsRayDistance;
+
+    [SerializeField] private LayerMask WorldMask;
+    
+    
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + (Vector3)(Vector2.down * _downwardsRayDistance));
     }
 
-    public enum MobilityState
+    // Update is called once per frame
+    void Update()
     {
+        /* x speed aanpassen
+         * state checken voor y spul
+         * x & y clampen naar max waardes
+         * translate naar nieuwe positie
+         * corrigeer naar juite positie
+         */
+
+
+
+        _velocityX += (MovementAcceleration * Input.GetAxis("Horizontal"));
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, _downwardsRayDistance,WorldMask);
+
+        float correctX = 0, correctY = 0;
+
+        if(hit.collider != null)
+        {
+            if (_velocityY < 1 && hit.collider.CompareTag("WorldLayout"))
+            {
+                _mobilityState = MobilityStates.OnGround;
+                _velocityY = 0;
+            }
+        }
+        else
+        {
+            _mobilityState = MobilityStates.InAir;
+        }
+
+        switch (_mobilityState)
+        {
+            case MobilityStates.OnGround:
+                _velocityX *= Friction;
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    _mobilityState = MobilityStates.InAirSlowDescent;
+                    _velocityY = JumpPower;
+                }
+                break;
+
+            case MobilityStates.InAirSlowDescent:
+                _velocityY -= Gravity / 2;
+
+                if (Input.GetKeyUp(KeyCode.Space))
+                {
+                    _mobilityState = MobilityStates.InAir;
+                }
+                break;
+            case MobilityStates.InAir:
+                _velocityY -= Gravity;
+                break;
+        }
+
+        _velocityX = Mathf.Clamp(_velocityX, -_maxVelocityX, _maxVelocityX);
+        _velocityY = Mathf.Clamp(_velocityY, -_maxVelocityY, _maxVelocityY);
+
+        transform.position += new Vector3(_velocityX + correctX, +correctY) * Time.deltaTime;
+    }
+
+    public enum MobilityStates
+    {
+        InAirSlowDescent,
         InAir,
         OnGround,
         OnWall
@@ -38,4 +105,5 @@ public class PlayerController : MonoBehaviour {
         Damaged,
         Invincible
     }
+    
 }
---
+++ Game Mechanics/Assets/Scripts/Racycast2DField.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Racycast2DField
+{
+    public float Distance => _distance;
+    [SerializeField] private float _distance;
+    public float Width => _width;
+    [SerializeField] private float _width;
+    public int RayCount => _rayCount;
+    [Range(1, 10)]
+    [SerializeField] private int _rayCount;
+    public Vector2 Offset => _offset;
+    [SerializeField] private Vector2 _offset;
+    public Vector2 Direction => _direction;
+    [SerializeField] private Vector2 _direction;
+
+    public void DrawGizmo(Vector2 origin)
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(new Vector2(-_offset.y*(_width/2),_offset.x *(_width/2)) + origin, new Vector2(_offset.y * (_width / 2), -_offset.x * (_width / 2)) + origin );
+
+
+        Gizmos.color = Color.red;
+        for (int i = 0; i < _rayCount; i++)
+        {
+            
+        }
+    }
+}
