3cf7a41aff89b7c30b2b37474056bc6a54251625
Pheubel <luca.guagliardo@live.nl>
2018-10-12 13:27:15 +0200

    made project win 10 compatible, player controller is smooth af now

--- Game Mechanics/Assets/Scripts/PlayerController.cs
+++ Game Mechanics/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections;
+﻿#pragma warning disable 0649
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]private float MovementAcceleration;
+
+    [SerializeField] private float MovementAcceleration;
     public float JumpPower;
     public float Gravity;
     [SerializeField] private MobilityStates _mobilityState;
     [Range(0, 1), Tooltip("Determines how much energy is conserved when walking along the ground.")]
     public float Friction;
+
 
     [SerializeField] private float _velocityX;
     [SerializeField] private float _velocityY;
@@ -17,54 +21,38 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _maxVelocityX;
     [SerializeField] private float _maxVelocityY;
 
-    [Tooltip("casts ray downward to check ground!")]
-    [SerializeField] private float _downwardsRayDistance;
-
     [SerializeField] private LayerMask WorldMask;
-    
-    
+
+    [SerializeField] private Racycast2DField _rayFieldUp;
+    [SerializeField] private Racycast2DField _rayFieldDown;
+    [SerializeField] private Racycast2DField _rayFieldLeft;
+    [SerializeField] private Racycast2DField _rayFieldRight;
 
     private void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, transform.position + (Vector3)(Vector2.down * _downwardsRayDistance));
+        if (_velocityX > 0)
+            _rayFieldRight.DrawGizmo(transform.position, _velocityX * Time.deltaTime);
+        else if (_velocityX < 0)
+            _rayFieldLeft.DrawGizmo(transform.position, Mathf.Abs(_velocityX) * Time.deltaTime);
+
+        if (_velocityY > 0)
+            _rayFieldUp.DrawGizmo(transform.position, _velocityY * Time.deltaTime);
+        else if (_velocityY < 0)
+            _rayFieldDown.DrawGizmo(transform.position, Mathf.Abs(_velocityY) * Time.deltaTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        /* x speed aanpassen
-         * state checken voor y spul
-         * x & y clampen naar max waardes
-         * translate naar nieuwe positie
-         * corrigeer naar juite positie
-         */
-
-
+        Vector2 currentPosition = transform.position;
 
         _velocityX += (MovementAcceleration * Input.GetAxis("Horizontal"));
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, _downwardsRayDistance,WorldMask);
-
-        float correctX = 0, correctY = 0;
-
-        if(hit.collider != null)
-        {
-            if (_velocityY < 1 && hit.collider.CompareTag("WorldLayout"))
-            {
-                _mobilityState = MobilityStates.OnGround;
-                _velocityY = 0;
-            }
-        }
-        else
-        {
-            _mobilityState = MobilityStates.InAir;
-        }
+        _velocityX *= Friction;
 
         switch (_mobilityState)
         {
             case MobilityStates.OnGround:
-                _velocityX *= Friction;
+
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
                     _mobilityState = MobilityStates.InAirSlowDescent;
@@ -85,10 +73,76 @@ public class PlayerController : MonoBehaviour
                 break;
         }
 
-        _velocityX = Mathf.Clamp(_velocityX, -_maxVelocityX, _maxVelocityX);
-        _velocityY = Mathf.Clamp(_velocityY, -_maxVelocityY, _maxVelocityY);
+        var deltaVelocity = new Vector2(_velocityX = Mathf.Clamp(_velocityX, -_maxVelocityX, _maxVelocityX),
+        _velocityY = Mathf.Clamp(_velocityY, -_maxVelocityY, _maxVelocityY)) * Time.deltaTime;
 
-        transform.position += new Vector3(_velocityX + correctX, +correctY) * Time.deltaTime;
+        float correctX = 0, correctY = 0;
+
+        if (_velocityX > 0)
+        {
+            RaycastHit2D[] rightHit = _rayFieldRight.Cast(currentPosition, deltaVelocity.x, WorldMask);
+
+            if (rightHit.HitTag(Helper.WorldLayOut, out correctX))
+            {
+                _velocityX = 0;
+            }
+
+            else
+            {
+                correctX = deltaVelocity.x;
+            }
+        }
+
+        else if (_velocityX < 0)
+        {
+            RaycastHit2D[] leftHit = _rayFieldLeft.Cast(currentPosition, Mathf.Abs(deltaVelocity.x), WorldMask);
+
+            if (leftHit.HitTag(Helper.WorldLayOut, out correctX))
+            {
+                _velocityX = 0;
+                correctX *= -1;
+            }
+
+            else
+            {
+                correctX = deltaVelocity.x;
+            }
+        }
+
+        if (_velocityY > 0)
+        {
+            RaycastHit2D[] upHit = _rayFieldUp.Cast(currentPosition, deltaVelocity.y, WorldMask);
+
+            if (upHit.HitTag(Helper.WorldLayOut, out correctY))
+            {
+                _mobilityState = MobilityStates.InAir;
+                _velocityY = 0;
+            }
+
+            else
+            {
+                correctY = deltaVelocity.y;
+            }
+        }
+
+        RaycastHit2D[] downHit = _rayFieldDown.Cast(currentPosition, Mathf.Abs(deltaVelocity.y), WorldMask);
+
+        if (_velocityY < 0 && downHit.HitTag(Helper.WorldLayOut, out correctY))
+        {
+            _mobilityState = MobilityStates.OnGround;
+            _velocityY = 0;
+            correctY *= -1;
+        }
+
+        else
+        {
+            if (_velocityY == 0)
+                _mobilityState = MobilityStates.InAir;
+            correctY = deltaVelocity.y;
+        }
+
+
+        transform.position = currentPosition + new Vector2(correctX, correctY);
     }
 
     public enum MobilityStates
@@ -105,5 +159,5 @@ public class PlayerController : MonoBehaviour
         Damaged,
         Invincible
     }
-    
+
 }
--- Game Mechanics/Assets/Scripts/Racycast2DField.cs
+++ Game Mechanics/Assets/Scripts/Racycast2DField.cs
@@ -3,30 +3,503 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public struct Racycast2DField
+public class Racycast2DField
 {
-    public float Distance => _distance;
-    [SerializeField] private float _distance;
     public float Width => _width;
     [SerializeField] private float _width;
-    public int RayCount => _rayCount;
-    [Range(1, 10)]
-    [SerializeField] private int _rayCount;
     public Vector2 Offset => _offset;
     [SerializeField] private Vector2 _offset;
-    public Vector2 Direction => _direction;
-    [SerializeField] private Vector2 _direction;
+    public Directions Direction => _direction;
+    [SerializeField] private Directions _direction;
+    [SerializeField] private Densities _density = Densities.Low;
 
-    public void DrawGizmo(Vector2 origin)
+
+    public void DrawGizmo(Vector2 origin, float distance)
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(new Vector2(-_offset.y*(_width/2),_offset.x *(_width/2)) + origin, new Vector2(_offset.y * (_width / 2), -_offset.x * (_width / 2)) + origin );
 
+        float step = (_width * 2) / ((int)_density - 1);
 
-        Gizmos.color = Color.red;
-        for (int i = 0; i < _rayCount; i++)
+        switch (_direction)
         {
-            
+            case Directions.Up:
+                // draw base line
+                Gizmos.DrawLine(
+                    new Vector3(origin.x + _offset.x - _width, origin.y + _offset.y),
+                    new Vector3(origin.x + _offset.x + _width, origin.y + _offset.y));
+
+                Gizmos.color = Color.red;
+
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    Gizmos.DrawLine(
+                        new Vector2(
+                            origin.x + Offset.x - _width + (step * i),
+                            origin.y + Offset.y),
+                        (Vector2.up * distance) + new Vector2(
+                            origin.x + Offset.x - _width + (step * i),
+                            origin.y + Offset.y));
+                }
+                break;
+
+            case Directions.Down:
+                // draw base line
+                Gizmos.DrawLine(
+                    new Vector3(origin.x + _offset.x - _width, origin.y + _offset.y),
+                    new Vector3(origin.x + _offset.x + _width, origin.y + _offset.y));
+
+                Gizmos.color = Color.red;
+
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    Gizmos.DrawLine(
+                        new Vector2(
+                            origin.x + Offset.x - _width + (step * i),
+                            origin.y + Offset.y),
+                        (Vector2.down * distance) + new Vector2(
+                            origin.x + Offset.x - _width + (step * i),
+                            origin.y + Offset.y));
+                }
+                break;
+
+            case Directions.Left:
+                // draw base line
+                Gizmos.DrawLine(
+                    new Vector3(origin.x + _offset.x, origin.y + _offset.y - _width),
+                    new Vector3(origin.x + _offset.x, origin.y + _offset.y + _width));
+
+                Gizmos.color = Color.red;
+
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    Gizmos.DrawLine(
+                        new Vector2(
+                            origin.x + Offset.x,
+                            origin.y + Offset.y - _width + (step * i)),
+                        (Vector2.left * distance) + new Vector2(
+                            origin.x + Offset.x,
+                            origin.y + Offset.y - _width + (step * i)));
+                }
+                break;
+
+            case Directions.Right:
+                // draw base line
+                Gizmos.DrawLine(
+                    new Vector3(origin.x + _offset.x, origin.y + _offset.y - _width),
+                    new Vector3(origin.x + _offset.x, origin.y + _offset.y + _width));
+
+                Gizmos.color = Color.red;
+
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    Gizmos.DrawLine(
+                        new Vector2(
+                            origin.x + Offset.x,
+                            origin.y + Offset.y - _width + (step * i)),
+                        (Vector2.right * distance) + new Vector2(
+                            origin.x + Offset.x,
+                            origin.y + Offset.y - _width + (step * i)));
+                }
+                break;
+
+            default: break;
         }
+    }
+
+    /// <summary> Casts a set of rays from the origin in the set direction and return hit objects.</summary>
+    public RaycastHit2D[] Cast(Vector2 origin, float distance, int layerMask)
+    {
+        RaycastHit2D[] hits = new RaycastHit2D[(int)_density];
+
+        float step = (_width * 2) / ((int)_density - 1);
+
+        switch (_direction)
+        {
+            case Directions.Up:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    hits[i] = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x - _width + (step * i),
+                            origin.y + Offset.y),
+                        Vector2.up,
+                        distance,
+                        layerMask);
+                }
+                break;
+            case Directions.Down:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    hits[i] = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x - _width + (step * i),
+                            origin.y + Offset.y),
+                        Vector2.down,
+                        distance,
+                        layerMask);
+                }
+                break;
+            case Directions.Left:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    hits[i] = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x,
+                            origin.y + Offset.y - _width + (step * i)),
+                        Vector2.left,
+                        distance,
+                        layerMask);
+                }
+                break;
+            case Directions.Right:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    hits[i] = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x,
+                            origin.y + Offset.y - _width + (step * i)),
+                        Vector2.right,
+                        distance,
+                        layerMask);
+                }
+                break;
+
+            default:
+                throw new System.Exception("Raycast field has no valid direction.");
+        }
+
+        Debug.Log(hits);
+
+        return hits;
+    }
+
+    /// <summary> Casts a set of rays from the origin in the set direction.</summary>
+    public bool CastHitTag(Vector2 origin, float distance, string tag, int layerMask)
+    {
+        float step = (_width * 2) / ((int)_density - 1);
+
+        switch (_direction)
+        {
+            case Directions.Up:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x - _width + (step * i),
+                            origin.y + Offset.y),
+                        Vector2.up,
+                        distance,
+                        layerMask);
+
+                    if (hit.collider == null && !hit.collider.CompareTag(tag))
+                        continue;
+
+                    return true;
+                }
+                break;
+            case Directions.Down:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x - _width + (step * i),
+                            origin.y + Offset.y),
+                        Vector2.down,
+                        distance,
+                        layerMask);
+
+                    if (hit.collider == null && !hit.collider.CompareTag(tag))
+                        continue;
+
+                    return true;
+                }
+                break;
+            case Directions.Left:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x,
+                            origin.y + Offset.y - _width + (step * i)),
+                        Vector2.left,
+                        distance,
+                        layerMask);
+
+                    if (hit.collider == null && !hit.collider.CompareTag(tag))
+                        continue;
+
+                    return true;
+                }
+                break;
+            case Directions.Right:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x,
+                            origin.y + Offset.y - _width + (step * i)),
+                        Vector2.right,
+                        distance,
+                        layerMask);
+
+                    if (hit.collider == null && !hit.collider.CompareTag(tag))
+                        continue;
+
+                    return true;
+                }
+                break;
+
+            default:
+                throw new System.Exception("Raycast field has no valid direction.");
+        }
+
+        return false;
+    }
+
+
+    /// <summary> Casts a set of rays from the origin in the set direction.</summary>
+    public bool CastHasHit(Vector2 origin, float distance, out float correction, int layerMask)
+    {
+        bool hasHit = false;
+
+        correction = 0;
+
+        float step = (_width * 2) / ((int)_density - 1);
+
+        switch (_direction)
+        {
+            case Directions.Up:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x - _width + (step * i),
+                            origin.y + Offset.y),
+                        Vector2.up,
+                        distance,
+                        layerMask);
+
+                    if (hit.collider == null)
+                        continue;
+
+                    correction = Mathf.Max(correction, hit.distance);
+                    hasHit = true;
+                }
+                break;
+            case Directions.Down:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x - _width + (step * i),
+                            origin.y + Offset.y),
+                        Vector2.down,
+                        distance,
+                        layerMask);
+
+                    if (hit.collider == null)
+                        continue;
+
+                    correction = Mathf.Max(correction, hit.distance);
+                    hasHit = true;
+                }
+                break;
+            case Directions.Left:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x,
+                            origin.y + Offset.y - _width + (step * i)),
+                        Vector2.left,
+                        distance,
+                        layerMask);
+
+                    if (hit.collider == null)
+                        continue;
+
+                    correction = Mathf.Max(correction, hit.distance);
+                    hasHit = true;
+                }
+                break;
+            case Directions.Right:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x,
+                            origin.y + Offset.y - _width + (step * i)),
+                        Vector2.right,
+                        distance,
+                        layerMask);
+
+                    if (hit.collider == null)
+                        continue;
+
+                    correction = Mathf.Max(correction, hit.distance);
+                    hasHit = true;
+                }
+                break;
+
+            default:
+                throw new System.Exception("Raycast field has no valid direction.");
+        }
+
+        return hasHit;
+    }
+
+    /// <summary> Casts a set of rays from the origin in the set direction.</summary>
+    public bool CastHasHitTag(Vector2 origin, float distance, string tag, out float correction, int layerMask)
+    {
+        bool hasHit = false;
+
+        correction = 0;
+
+        float step = (_width * 2) / ((int)_density - 1);
+
+        switch (_direction)
+        {
+            case Directions.Up:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x - _width + (step * i),
+                            origin.y + Offset.y),
+                        Vector2.up,
+                        distance,
+                        layerMask);
+
+                    if (hit.collider == null && !hit.collider.CompareTag(tag))
+                        continue;
+
+                    correction = Mathf.Max(correction, hit.distance);
+                    hasHit = true;
+                }
+                break;
+            case Directions.Down:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x - _width + (step * i),
+                            origin.y + Offset.y),
+                        Vector2.down,
+                        distance,
+                        layerMask);
+
+                    if (hit.collider == null && !hit.collider.CompareTag(tag))
+                        continue;
+
+                    correction = Mathf.Max(correction, hit.distance);
+                    hasHit = true;
+                }
+                break;
+            case Directions.Left:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x,
+                            origin.y + Offset.y - _width + (step * i)),
+                        Vector2.left,
+                        distance,
+                        layerMask);
+
+                    if (hit.collider == null && !hit.collider.CompareTag(tag))
+                        continue;
+
+                    correction = Mathf.Max(correction, hit.distance);
+                    hasHit = true;
+                }
+                break;
+            case Directions.Right:
+                for (int i = 0; i < (int)_density; i++)
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(
+                        new Vector2(
+                            origin.x + Offset.x,
+                            origin.y + Offset.y - _width + (step * i)),
+                        Vector2.right,
+                        distance,
+                        layerMask);
+
+                    if (hit.collider == null && !hit.collider.CompareTag(tag))
+                        continue;
+
+                    correction = Mathf.Max(correction, hit.distance);
+                    hasHit = true;
+                }
+                break;
+
+            default:
+                throw new System.Exception("Raycast field has no valid direction.");
+        }
+
+        return hasHit;
+    }
+
+    public enum Directions
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    public enum Densities
+    {
+        Low = 3,
+
+    }
+}
+
+public static class RaycastHit2dArrayExtention
+{
+    public static RaycastHit2D ReturnClosestHit(this RaycastHit2D[] hits)
+    {
+        RaycastHit2D closestHit = hits[0];
+
+        for (int i = 1; i < hits.Length; i++)
+        {
+            if (hits[i].distance < closestHit.distance)
+                closestHit = hits[i];
+        }
+
+        return closestHit;
+    }
+
+    public static bool HitTag(this RaycastHit2D[] hits, string tagName)
+    {
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i].collider == null)
+                continue;
+            if (hits[i].collider.CompareTag(tagName))
+                return true;
+        }
+        return false;
+    }
+
+    public static bool HitTag(this RaycastHit2D[] hits, string tagName, out float correction)
+    {
+        bool hasHit = false;
+        correction = 0;
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i].collider == null)
+                continue;
+
+            if (hits[i].collider.CompareTag(tagName))
+            {
+                correction = Mathf.Max(correction, hits[i].distance);
+                hasHit = true;
+
+                Debug.Log($"Hit {tagName}");
+            }
+        }
+
+        return hasHit;
     }
 }
